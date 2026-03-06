753a7fb70254834c4111379bf3aa098d74449917
Luano Cesar <luanocesar@gmail.com>
2026-03-06 12:54:47 -0400

    feat: DTO implementation

---
+++ backend/src/Web/DTOs/User.cs
@@ -0,0 +1,6 @@
+namespace backend.Web.DTOs
+{
+    public record LoginRequest(string Email, string Password);
+    public record UserRequest(string Name, string Email, string Password);
+    public record UserResponse(int Id, string Name, string Email, DateTime CreatedAt);
+}
