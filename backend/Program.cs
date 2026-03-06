6c00005e834257bc90ec2ac4b11903392805b370
Luano Cesar <luanocesar@gmail.com>
2026-03-06 13:05:57 -0400

    chore: settings adjustment for Docker Up

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-using backend.Data;
+using backend.Infrastructure.Data;
+using backend.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(
-    options=> options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+// Setting Up Database
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddAppServices(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -13,14 +17,20 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AngularApp");
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
+
+app.ApplyMigrationsAndSeed();
 
 app.Run();
